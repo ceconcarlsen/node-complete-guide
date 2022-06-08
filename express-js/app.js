594d27de37599d777081c20272e42a7724a26df6
Gabriel Cecon Carlsen <cecon.carlsen@unesp.br>
2022-06-08 02:10:53 -0300

    feat: lesson 5

--- express-js/app.js
+++ express-js/app.js
@@ -1,14 +1,26 @@
+const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const rootDir = require("./util/path");
+
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
-  console.log("middleware 1");
-  next();
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+/*
+Delegating responsabilities.
+ */
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log("middleware 2");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(port);
